5f1d497a31699d27b3e8e8b6aa48141c882486cc
tcodling <thomas.codling@outlook.com>
2020-04-09 13:34:14 +1200

    setup files with template

---
+++ client/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+// import Nav from './Nav'
+
+const App = () => {
+  return (
+    <>
+    <h1>React development has begun!</h1>
+    {/* <Router>
+      <Route path='/' component={Nav} />
+      <Route exact path="/" component={Home} />
+      <Route exact path='/list/:rank' component={Rank} />
+      <Route exact path='/rank/:rank/:name' component={Description} />
+    </Router> */}
+    </>
+  )
+}
+
+export default App
---
+++ client/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+// const Nav = () => {
+//   return (
+//     <>
+//     <h1>Nav</h1>
+//     <ul>
+//         {Object.keys(data).map(name => <li><Link to={`/list/${name}`}>{name}</Link></li>)}
+//     </ul>
+//     </>
+//   )
+// }
+
+export default Nav
