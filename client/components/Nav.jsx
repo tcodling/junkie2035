6337134e00a1846f016c47e7db5cb88c78d07aae
tcodling <thomas.codling@outlook.com>
2020-04-16 13:54:14 +1200

    add gradient to background

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <>
+    <div className='chapterList'>
     <h1>Chapters</h1>
     <ul>
         <li><Link to='/chapter/1'>Chapter 1</Link></li>
         <li><Link to='/chapter/2'>Chapter 2</Link></li>
         <li><Link to='/chapter/3'>Chapter 3</Link></li>
     </ul>
-    </>
+    </div>
   )
 }
 
