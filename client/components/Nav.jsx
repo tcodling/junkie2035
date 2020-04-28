b1126244e013be34d4a8a99e2be446b9e41ef9a1
tcodling <thomas.codling@outlook.com>
2020-04-28 16:48:33 +1200

    fetch nav content from api

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
-
-const Nav = () => {
+const Nav = (props) => {
   return (
     <div className='chapterList'>
     <h1>Chapters</h1>
     <ul>
-        <li><Link to='/chapter/1'>Chapter 1</Link></li>
-        <li><Link to='/chapter/2'>Chapter 2</Link></li>
-        <li><Link to='/chapter/3'>Chapter 3</Link></li>
+    {props.chapters.map(chapter => <li>
+      <Link to={`/chapter/${chapter.fields.id}`}>{chapter.fields.title}</Link>
+    </li>)}
     </ul>
     </div>
   )
 }
 
-export default Nav
+function mapStateToProps(state) {
+  return {
+    chapters: state.chapters
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
