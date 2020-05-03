b53616b095a48692657b41256de6b90fbda9b776
tcodling <thomas.codling@outlook.com>
2020-05-04 11:47:29 +1200

    render chapter content without danger

--- client/components/Chapter.jsx
+++ client/components/Chapter.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
-
-// function createMarkup() {
-//   return {__html: chapter.fields.content};
-// }
-
 const Chapter = (props) => {
   const chapter = props.chapters.find(chapter => chapter.fields.id == props.match.params.id)
   return (
@@ -14,11 +9,15 @@ const Chapter = (props) => {
     {chapter ? (
       <>
       <h1>{chapter.fields.title}</h1>
-      {/* <p>{chapter.fields.content}</p> */}
-      <p dangerouslySetInnerHTML={{__html: chapter.fields.content.replace(/\n/g, "<br />")}}></p>
+      <p>
+      {chapter.fields.content.split("\n").map(line => {
+        return (
+          <span>{line}<br/></span>
+        )
+      })}
+      </p>
       </>
     ) : 'loading'}
-    {/* replace(/\n/g, "<br />"); */}
     </div>
   )
 }
